3455c01e5b1b6c8bbbb8867b1899b77409227c4d
Kaushikshikari54 <kaushikshikari54@gmail.com>
2025-08-15 18:59:03 +0530

    Update app-train.py

--- app-train.py
+++ app-train.py
@@ -1,19 +1,1 @@
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import joblib
 
-# Load dataset
-digits = load_digits()
-X, y = digits.data, digits.target
-
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Train model
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-# Save the model
-joblib.dump(model, 'digit_classifier.pkl')
-print("Model saved as digit_classifier.pkl")
